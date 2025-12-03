cfcebc2ca02e12209cef8b7508b1a9c05855e26e
Usman Ghani <rajausman127official@gmail.com>
2025-12-03 16:45:11 +0500

    Removed @react-native/new-app-screen

--- App.tsx
+++ App.tsx
@@ -5,11 +5,9 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
-  useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 
 function App() {
@@ -18,28 +16,42 @@ function App() {
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      <View style={[styles.container, isDarkMode ? dark.container : light.container]}>
+        <Text style={[styles.text, isDarkMode ? dark.text : light.text]}>Hello World</Text>
+      </View>
     </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+  text: {
+    fontSize: 22,
+    fontWeight: 'bold'
+  }
 });
+
+const light = StyleSheet.create({
+  container: {
+    backgroundColor: 'white'
+  },
+  text: {
+    color: 'black'
+  }
+});
+
+const dark = StyleSheet.create({
+  container: {
+    backgroundColor: 'black'
+  },
+  text: {
+    color: 'white'
+  }
+});
+
 
 export default App;
