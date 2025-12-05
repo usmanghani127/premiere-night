7ef1c91c302675b363e639dde2c5d3906d0cb6a4
Usman Ghani <rajausman127official@gmail.com>
2025-12-06 00:10:08 +0500

    Fixed search bar ui glitch

--- src/screens/Spotlight/Header.tsx
+++ src/screens/Spotlight/Header.tsx
@@ -2,9 +2,7 @@ import { SearchBar } from '@common/components/SearchBar';
 import { AppNavigatorProps } from '@navigation/types';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { useRef } from 'react';
 import { useTranslation } from 'react-i18next';
-import { TextInput } from 'react-native';
 import { HeaderRow } from './styles';
 
 export const Header = () => {
@@ -12,14 +10,11 @@ export const Header = () => {
   const navigation =
     useNavigation<NativeStackNavigationProp<AppNavigatorProps, 'Spotlight'>>();
 
-  const searchBarRef = useRef<TextInput>(null);
-
   return (
     <HeaderRow>
       <SearchBar
-        ref={searchBarRef}
         placeholder={t('Search.placeholder')}
-        disabled={true}
+        editable={false}
         onPress={() => navigation.navigate('Search')}
       />
     </HeaderRow>
